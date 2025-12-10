d830b01a7d5eabf05fe4007166ae7e4da4493c97
B00142713 Abel Ikhaguebor <b00142713@mytudublin.ie>
2025-12-10 04:28:06 +0000

    Syntax fix to sql

--- sql/createDBV1.sql
+++ sql/createDBV1.sql
@@ -1,22 +1,25 @@
-drop database if exists bookappoinment;
-create database bookappoinment;
-use bookappoinment;
+drop database if exists bookappointment;
+create database bookappointment;
+use bookappointment;
+
 create table appointmentbooking (
     id int auto_increment,
     bookingtime varchar(30),
     bookingdate varchar(30),
-    treatment varchar(30)
-    primary key(id)
+    treatment varchar(30),
+    primary key (id)
 );
+
 create table patientname (
     id int auto_increment,
     firstname varchar(30),
     surname varchar(30),
-    primary key(id)
+    primary key (id)
 );
+
 create table patientappointment (
     id int auto_increment,
     appointmentid int,
     patientid int,
-    primary key(id),
+    primary key (id)
 );
