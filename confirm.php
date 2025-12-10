9921ca6b63072f9f976b34f447fdac42a8736022
B00142713 Abel Ikhaguebor <b00142713@mytudublin.ie>
2025-12-10 04:32:20 +0000

    Confirm php done

--- confirm.php
+++ confirm.php
@@ -1,19 +1,16 @@
 <?php
 session_start();
 
-if (!isset($_POST['confirm'])) {
-    header("Location: appointmentForm.html");
-    exit;
-}
-
 $fn          = $_SESSION['patientFN'];
 $sn          = $_SESSION['patientSN'];
 $bookingDate = $_SESSION['bookingdate'];
 $bookingTime = $_SESSION['bookingtime'];
+$treatment   = $_SESSION['treatment'];
 
 include("dbcon.php");
 
-$sqlPatient = "INSERT INTO patientname (firstname, surname) VALUES ('$fn', '$sn')";
+$sqlPatient = "INSERT INTO patientname (firstname, surname)
+               VALUES ('$fn', '$sn')";
 
 if (mysqli_query($conn, $sqlPatient)) {
   echo "Patient was added successfully<br>";
@@ -21,7 +18,8 @@ if (mysqli_query($conn, $sqlPatient)) {
   echo "Error: " . $sqlPatient . "<br>" . mysqli_error($conn);
 }
 
-$sqlAppointment = "INSERT INTO appointmentbooking (bookingtime, bookingdate) VALUES ('$bookingTime', '$bookingDate')";
+$sqlAppointment = "INSERT INTO appointmentbooking (bookingtime, bookingdate, treatment)
+                   VALUES ('$bookingTime', '$bookingDate', '$treatment')";
 
 if (mysqli_query($conn, $sqlAppointment)) {
   echo "Appointment was booked successfully";
