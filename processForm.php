ed2bce8c34ef9fee2cc27a8a4aaf9b53a47c1dad
B00142713 Abel Ikhaguebor <b00142713@mytudublin.ie>
2025-12-10 03:25:45 +0000

    Added treatment dropdown menu to process form

--- processForm.php
+++ processForm.php
@@ -5,6 +5,7 @@ $_SESSION['patientFN']   = $_POST['firstname'];
 $_SESSION['patientSN']   = $_POST['surname'];
 $_SESSION['bookingdate'] = $_POST['bookingdate'];
 $_SESSION['bookingtime'] = $_POST['bookingtime'];
+$_SESSION['treatment']   = $_POST['treatment'];
 
 header("Location: confirm.php");
 
